9eb5bf8227577cbc0a6b040f6ce975bd36737502
Ruiqi-Xie <rx2171@columbia.edu>
2020-04-21 02:00:46 -0400

    main

---
+++ lib/Post_Process_P3.R
@@ -0,0 +1,15 @@
+library(pracma)
+K=function(x_i_t,x_j_t){
+  return (exp(2*(x_i_t%*%t(x_j_t)-1)))
+}
+
+X_mat=function(q){
+  X=apply(t(q), 2 ,function(x) x/Norm(x))
+  return (X)
+}
+
+svd_krr=function(n,lambda=0.5,X,y){
+  I=diag(n)
+  res=K(X,X)%*%solve(K(X,X)+lambda*I)%*%y
+  return (res)
+}
